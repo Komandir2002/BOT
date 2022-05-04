4c37860264182e806bd5e01883820a979dfdbd1c
Komandir2002 <temirlan2002@gmail.com>
2022-05-04 09:20:04 +0600

    mym

--- database/psql_db.py
+++ database/psql_db.py
@@ -7,17 +7,10 @@ cursor = db.cursor()
 def psql_create():
     try:
         cursor.execute("CREATE TABLE IF NOT EXISTS users (id TEXT, username TEXT, fullname TEXT);")
+        cursor.execute("CREATE TABLE IF NOT EXISTS film (title TEXT, discription TEXT);")
         db.commit()
     except:
         cursor.execute("rollback")
         cursor.execute("CREATE TABLE IF NOT EXISTS users (id TEXT, username TEXT, fullname TEXT);")
-        db.commit()
-
-def psql_film_create():
-    try:
-        cursor.execute("CREATE TABLE IF NOT EXISTS film (title TEXT, discription TEXT);")
-        db.commit()
-    except:
-        cursor.execute("filmback")
         cursor.execute("CREATE TABLE IF NOT EXISTS film (title TEXT, discription TEXT);")
         db.commit()
